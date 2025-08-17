59e4875677fa288533c814fa1c94bbc2981a903b
hazi000 <alhazierratag12@gmail.com>
2025-08-17 15:24:17 +0800

    Remove SMTP configuration from config.php for security reasons

--- config.php
+++ config.php
@@ -6,11 +6,5 @@ $database = 'barangay_management';
 
 $connection = mysqli_connect($host, $username, $password, $database);
 
-define('SMTP_HOST', 'smtp.gmail.com');
-define('SMTP_USER', 'cawitbarangay@gmail.com');
-define('SMTP_PASS', 'barangay1234');
-define('SMTP_SECURE', 'tls'); // or 'ssl'
-define('SMTP_PORT', 587); // or 465 for SSL
-define('SMTP_FROM_EMAIL', 'cawitbarangay@gmail.com');
-define('SMTP_FROM_NAME', 'Cawit Barangay');
+
 ?>
